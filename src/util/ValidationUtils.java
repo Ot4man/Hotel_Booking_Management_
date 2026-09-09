08919d12177e3656e599afd40a798399e3712565
Ot4man <melloukiotmane@gmail.com>
2026-09-09 14:16:38 +0100

    feat:add validation forme

--- src/util/ValidationUtils.java
+++ src/util/ValidationUtils.java
@@ -1,1 +1,20 @@
 package util;
+
+public class ValidationUtils {
+
+    public boolean isValidName(String name) {
+        return name != null && !name.isBlank() && name.matches("[a-zA-Z]");
+    }
+
+    public boolean isValidEmail(String email) {
+        return email != null && email.matches(".+@.+\\..+");
+    }
+
+    public boolean isValidPhone(String phone) {
+        return phone != null && phone.matches("[0-9]+") && phone.length() >= 8;
+    }
+
+    public boolean isValidPassword(String password) {
+        return password != null && password.length() >= 8;
+    }
+}

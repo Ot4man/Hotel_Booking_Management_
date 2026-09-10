1390c7e7aa2bc3f48a3f2827cbcb9a46643fae04
Ot4man <melloukiotmane@gmail.com>
2026-09-10 09:38:28 +0100

    feat:add Exception of existant mails

--- src/exception/EmailAlreadyExistsException.java
+++ src/exception/EmailAlreadyExistsException.java
@@ -1,1 +1,7 @@
 package exception;
+
+public class EmailAlreadyExistsException extends RuntimeException {
+    public EmailAlreadyExistsException(String message) {
+        super(message);
+    }
+}

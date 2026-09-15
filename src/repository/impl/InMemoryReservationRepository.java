4ca1f1412d29307a445a90af7fc4c7830ca542c5
Ot4man <melloukiotmane@gmail.com>
2026-09-15 23:02:15 +0100

    feat: add Inmemory files

--- src/repository/impl/InMemoryReservationRepository.java
+++ src/repository/impl/InMemoryReservationRepository.java
@@ -1,1 +1,64 @@
 package repository.impl;
+
+import model.Reservation;
+import repository.ReservationRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public class InMemoryReservationRepository implements ReservationRepository {
+
+    private final List<Reservation> reservations = new ArrayList<>();
+
+    @Override
+    public void save(Reservation reservation) {
+        reservations.add(reservation);
+    }
+
+    @Override
+    public Optional<Reservation> findByid(UUID id) {
+        for (Reservation reservation : reservations) {
+            if (reservation.getId().equals(id)) {
+                return Optional.of(reservation);
+            }
+        }
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Reservation> findByUserId(UUID userId) {
+        List<Reservation> result = new ArrayList<>();
+        for (Reservation reservation : reservations) {
+            if (reservation.getUserId().equals(userId)) {
+                result.add(reservation);
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public List<Reservation> findRoomByNumber(String roomNumber) {
+        List<Reservation> result = new ArrayList<>();
+        for (Reservation reservation : reservations) {
+            if (reservation.getRoomNumber().equals(roomNumber)) {
+                result.add(reservation);
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public Optional<Reservation> findByCode(String reservationCode) {
+
+        for (Reservation reservation : reservations) {
+
+            if (reservation.getReservationCode().equals(reservationCode)) {
+                return Optional.of(reservation);
+            }
+        }
+
+        return Optional.empty();
+    }
+}
--- src/repository/impl/InMemoryRoomRepository.java
+++ src/repository/impl/InMemoryRoomRepository.java
@@ -1,1 +1,32 @@
 package repository.impl;
+
+import model.Room;
+import repository.RoomRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InMemoryRoomRepository implements RoomRepository {
+
+    private List<Room> rooms = new ArrayList<>();
+
+    public void save(Room room) {
+        rooms.add(room);
+    }
+
+    @Override
+    public Room findByRoomNumber(String roomNumber) {
+        for (Room room : rooms) {
+            if (room.getRoomNumber().equals(roomNumber)) {
+                return room;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public List<Room> findAll() {
+        return rooms;
+    }
+
+}

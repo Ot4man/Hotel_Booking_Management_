58796de5b573e591cc354e97b185fc5a1cf89b1e
Ot4man <melloukiotmane@gmail.com>
2026-09-15 23:17:51 +0100

    feat :add Reservation repository

--- src/repository/ReservationRepository.java
+++ src/repository/ReservationRepository.java
@@ -1,1 +1,20 @@
 package repository;
+
+import model.Reservation;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.Optional;
+
+public interface ReservationRepository {
+
+    void save(Reservation reservation);
+
+    Optional<Reservation> findByid(UUID id);
+
+    List<Reservation> findByUserId(UUID userId);
+
+    List<Reservation> findRoomByNumber(String roomNumber);
+
+    Optional<Reservation> findByCode(String reservationCode);
+}
